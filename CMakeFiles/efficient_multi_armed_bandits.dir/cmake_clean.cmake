4d82f1f21b3d683a6acd74cefc84f5fdcaf7620c
Mathias Ravn Tversted <Mathias@thesun.dk>
2022-02-03 17:47:31 +0100

    Added make files

---
+++ CMakeFiles/efficient_multi_armed_bandits.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/efficient_multi_armed_bandits.dir/algorithms/Exp3.cpp.o"
+  "CMakeFiles/efficient_multi_armed_bandits.dir/algorithms/Exp3.cpp.o.d"
+  "CMakeFiles/efficient_multi_armed_bandits.dir/experiments/exp3/mod2.cpp.o"
+  "CMakeFiles/efficient_multi_armed_bandits.dir/experiments/exp3/mod2.cpp.o.d"
+  "CMakeFiles/efficient_multi_armed_bandits.dir/main.cpp.o"
+  "CMakeFiles/efficient_multi_armed_bandits.dir/main.cpp.o.d"
+  "efficient_multi_armed_bandits"
+  "efficient_multi_armed_bandits.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/efficient_multi_armed_bandits.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
