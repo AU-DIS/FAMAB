c3e80f980ea25681cd453bb7806609eb04edac55
Mathias Ravn Tversted <Mathias@thesun.dk>
2022-02-04 11:18:30 +0100

    Cmake fixes

---
+++ cmake-build-debug/CMakeFiles/efficient_multi_armed_bandits.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/efficient_multi_armed_bandits.dir/algorithms/Exp3.cpp.o"
+  "CMakeFiles/efficient_multi_armed_bandits.dir/experiments/exp3/mod2.cpp.o"
+  "CMakeFiles/efficient_multi_armed_bandits.dir/main.cpp.o"
+  "efficient_multi_armed_bandits"
+  "efficient_multi_armed_bandits.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/efficient_multi_armed_bandits.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
