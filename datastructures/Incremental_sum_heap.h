015c1884084da8cc6c32b094db0eb5542235eb08
MARATVE <Mathias@thesun.dk>
2022-05-04 10:28:06 +0200

    Added m sampling

--- datastructures/Incremental_sum_heap.h
+++ datastructures/Incremental_sum_heap.h
@@ -2,7 +2,6 @@
 // Created by Mathias Ravn Tversted on 31/03/2022.
 //
 
-
 /*
  * Taken from Tim Viera at https://timvieira.github.io/blog/post/2016/11/21/heaps-for-incremental-computation/
  * We will use it as a heap to enable sampling
@@ -12,50 +11,63 @@
 #define EFFICIENT_MULTI_ARMED_BANDITS_INCREMENTAL_SUM_HEAP_H
 #include "cmath"
 #include "random"
+#include <stdio.h>
 
-class Incremental_sum_heap {
+class Incremental_sum_heap
+{
 
 public:
-    explicit Incremental_sum_heap(std::vector<double> &input_list) {
+    explicit Incremental_sum_heap(std::vector<double> &input_list)
+    {
         _n = input_list.size();
         _d = round(pow(2, log2(_n)));
         _heap = std::vector<double>(2 * _d, 0);
         int j = 0;
-        for (int i = _d; i < _d+_n; i++) {
+        for (int i = _d; i < _d + _n; i++)
+        {
             _heap[i] = input_list[j];
             j++;
         }
-        for (int i = _d-1; i >= 1; i--) {
-            _heap[i] = _heap[2*i] + _heap[2*i + 1];
+        for (int i = _d - 1; i >= 1; i--)
+        {
+            _heap[i] = _heap[2 * i] + _heap[2 * i + 1];
         }
     }
-    Incremental_sum_heap() = default;;
+    Incremental_sum_heap() = default;
+    ;
 
-    void update(int k, double v) {
+    void update(int k, double v)
+    {
         int d = _heap.size();
-        int i = d/2 + k;
+        int i = d / 2 + k;
         _heap[i] = v;
-        while (i > 0) {
+        while (i > 0)
+        {
             i /= 2;
-            _heap[i] = _heap[2*i] + _heap[2*i + 1];
+            _heap[i] = _heap[2 * i] + _heap[2 * i + 1];
         }
     }
 
-    double probability_of_choice(int i) {
-        return _heap[_heap.size()/2 + i];
+    double probability_of_choice(int i)
+    {
+        return _heap[_heap.size() / 2 + i];
     }
-    double max_element() {
+    double max_element()
+    {
         return _heap[1];
     }
 
-    int heap_sample() {
+    int heap_sample()
+    {
         double p = _uni(_random_gen) * _heap[1];
 
         int i = 1;
-        while (i < _d) {
+        while (i < _d)
+        {
             i *= 2;
             double left = _heap[i];
-            if (p > left) {
+            if (p > left)
+            {
                 p -= left;
                 i += 1;
             }
@@ -63,8 +75,30 @@ public:
         return i - _d;
     }
 
-    void print_weights() {
-        for (int i = _d; i < _d+_n; i++) {
+    std::vector<int> heap_sample(int k)
+    {
+        auto choices = std::vector<int>(k, 0);
+        auto priors = std::vector<double>(k, 0.);
+        auto z = std::vector<int>(k, 0);
+        for (int i = 0; i < k; i++)
+        {
+            int v = heap_sample();
+            choices[i] = v;
+            priors[i] = probability_of_choice(v);
+            z[i] = v;
+            update(v, 0);
+        }
+        for (int i = 0; i < k; i++)
+        {
+            update(choices[i], priors[i]);
+        }
+        return z;
+    }
+
+    void print_weights()
+    {
+        for (int i = _d; i < _d + _n; i++)
+        {
             std::cout << _heap[i] << ", ";
         }
         std::cout << std::endl;
@@ -78,6 +112,4 @@ private:
     std::mt19937 _random_gen = random_gen();
 };
 
-
-
-#endif //EFFICIENT_MULTI_ARMED_BANDITS_INCREMENTAL_SUM_HEAP_H
+#endif // EFFICIENT_MULTI_ARMED_BANDITS_INCREMENTAL_SUM_HEAP_H
